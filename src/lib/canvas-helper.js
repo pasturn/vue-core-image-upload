b6a820479fa71952fbde291850ba403530f5ccdf
pasturn <catcut@pasturn.com>
2017-11-13 18:22:02 +0800

    update

--- src/lib/canvas-helper.js
+++ src/lib/canvas-helper.js
@@ -20,6 +20,7 @@ export default {
     let size = src.size;
     const maxL = 1280;
     const self = this;
+    console.log(src)
     reader.onload = function(event) {
       const image = new Image();
       image.src = event.target.result;
@@ -40,6 +41,7 @@ export default {
           picH = imageH
           picW = imageW
         }
+        console.log(picH, picW)
         let newImageData, r = 1;
         const mimeType = self._getImageType(src.type);
         do{
@@ -53,9 +55,11 @@ export default {
             uBUffer[i] = code.charCodeAt[i]
           }
           var file = new File([uBUffer], filename, { type: mimeType })
+          console.log(file)
           size = file.size
           r -= 0.1
         } while (size > 1024000)
+        console.log(newImageData)
         callback(newImageData);
       }
     };
